d97a607fc2ce2540b5172edea5b2b1993e7151e8
sriharibanoth <sreeharibanoth1203@gmail.com>
2022-01-15 17:00:56 +0530

    Add project files.

---
+++ ADO.NETConnectionDB/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+namespace ADO.NETConnectionDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = null;
+            try
+            {
+                //Creating connection
+                con = new SqlConnection(@"data source=(localdb)\MSSQLLocalDB;database=NorthWind; integrated security=SSPI");
+
+                //writing sql query
+                SqlCommand cm = new SqlCommand("Select * from Employees", con);
+
+                //Opening Connection
+                con.Open();
+
+                //Executing the SQL query
+                SqlDataReader sdr = cm.ExecuteReader();
+                while (sdr.Read())
+                {
+                    Console.WriteLine(sdr["FirstName"] + " || " + sdr["LastName"] + " || " + sdr["Title"]);
+                }
+                Console.ReadLine();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Something is wrong " + e);
+            }
+        }
+    }
+}
